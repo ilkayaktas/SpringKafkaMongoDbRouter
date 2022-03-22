486d63b3840c927938225335ca5aa7d30ce67d9c
İlkay Aktaş <ilkayaktas@gmail.com>
2022-03-22 10:13:53 +0300

    Project is initialized.

---
+++ src/main/java/com/ilkayaktas/springkafkamongodbrouter/SpringKafkaMongoDbRouterApplication.java
@@ -0,0 +1,13 @@
+package com.ilkayaktas.springkafkamongodbrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMongoDbRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMongoDbRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilkayaktas/springkafkamongodbrouter/SpringKafkaMongoDbRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkayaktas.springkafkamongodbrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMongoDbRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
